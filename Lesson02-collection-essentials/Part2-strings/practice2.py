cee3add2bfa9704b8437f2cd40f490cbc05c7d4d
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-10-31 08:37:54 -0400

    ghjk

--- Lesson02-collection-essentials/Part2-strings/practice2.py
+++ Lesson02-collection-essentials/Part2-strings/practice2.py
@@ -7,3 +7,12 @@ def format_phone_number(phone):
         return 'invalid'
     return f'({cleaned[0:3]}) {cleaned[3:6]}-{cleaned[6:10]}'
 print(format_phone_number('555-123-4567'))
+
+def sanitize_filename(filename):
+    cl_filename = filename.lower().replace(' ','_')
+    for char in cl_filename:
+        if not char.isdigit() and not char.isalpha():
+            if not char in '._-':
+                cl_filename.replace(char,'')
+    if filename[-4] == '.':
+        
