72457acd9ac1593bc2137276ef3d79357e87e455
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-11-03 08:56:21 -0500

    jhtgrfdewsq

--- Lesson02-collection-essentials/Part2-strings/practice2.py
+++ Lesson02-collection-essentials/Part2-strings/practice2.py
@@ -8,11 +8,11 @@ def format_phone_number(phone):
     return f'({cleaned[0:3]}) {cleaned[3:6]}-{cleaned[6:10]}'
 print(format_phone_number('555-123-4567'))
 
-def sanitize_filename(filename):
-    cl_filename = filename.lower().replace(' ','_')
-    for char in cl_filename:
-        if not char.isdigit() and not char.isalpha():
-            if not char in '._-':
-                cl_filename.replace(char,'')
-    if filename[-4] == '.':
+# def sanitize_filename(filename):
+#     cl_filename = filename.lower().replace(' ','_')
+#     for char in cl_filename:
+#         if not char.isdigit() and not char.isalpha():
+#             if not char in '._-':
+#                 cl_filename.replace(char,'')
+#     if filename[-4] == '.':
         
